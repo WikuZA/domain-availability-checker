a87a75b71a72c1525725a28d291f446f95ffade4
WikuZA <wikusbasson18@gmail.com>
2022-04-29 22:32:30 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,90 +3,89 @@ import { domainList } from "../utils/tlds" // Import the domainList array
 
 export default function Home() {
   useEffect(() => {
-    return () => {
-      let domains = []; // Create an empty array
-      let warning = document.querySelector('.warning'); // Select the warning element
+    let domains = []; // Create an empty array
+    let warning = document.querySelector('.warning'); // Select the warning element
 
-      // Add event listener to the button
+    // Add event listener to the button
 
-      document.getElementById('js-form').addEventListener('submit', async (event) => {
-        event.preventDefault();
+    document.getElementById('js-form').addEventListener('submit', async (event) => {
+      event.preventDefault();
 
-        let classToRemove = document.getElementById('domain-availability'); // Select the element to remove
-        classToRemove.classList.remove('domain-taken', 'domain-free'); // Remove the class
-        classToRemove.innerHTML = ''; // Remove the innerHTML
 
-        let nameToCheck = document.getElementById('js-name').value.trim().replace(/\s/g, ""); // Gets the value of the input and removes spaces
+      let classToRemove = document.getElementById('domain-availability'); // Select the element to remove
+      classToRemove.classList.remove('domain-taken', 'domain-free'); // Remove the class
+      classToRemove.innerHTML = ''; // Remove the innerHTML
 
-        // Checks if the domain entered is greater than 1 character
+      let nameToCheck = document.getElementById('js-name').value.trim().replace(/\s/g, ""); // Gets the value of the input and removes spaces
 
-        if (nameToCheck.length < 2) {
-          warning.classList.remove('hidden'); // Show the warning
-          return
-        } else {
-          warning.classList.add('hidden'); // Hide the warning
-        }
+      // Checks if the domain entered is greater than 1 character
 
-        let domainName = nameToCheck.slice(0, nameToCheck.indexOf('.')); // Gets the domain name
-        let domainExtention = nameToCheck.slice(nameToCheck.indexOf('.')); // Gets the domain extention
+      if (nameToCheck.length < 2) {
+        warning.classList.remove('hidden'); // Show the warning
+        return
+      } else {
+        warning.classList.add('hidden'); // Hide the warning
+      }
 
-        // Checks if there is a domain extention ie. .com
-        // If there is no domain extention, it will add .com to the end of the domain name
+      let domainName = nameToCheck.slice(0, nameToCheck.indexOf('.')); // Gets the domain name
+      let domainExtention = nameToCheck.slice(nameToCheck.indexOf('.')); // Gets the domain extention
 
-        if (!nameToCheck.includes('.')) {
-          domainName = nameToCheck;
-          domainExtention = '.com';
-        }
+      // Checks if there is a domain extention ie. .com
+      // If there is no domain extention, it will add .com to the end of the domain name
 
-        // Checks if the domain name is greater than 1 character
+      if (!nameToCheck.includes('.')) {
+        domainName = nameToCheck;
+        domainExtention = '.com';
+      }
 
-        if (domainName.length < 2) {
-          warning.classList.remove('hidden'); // Show the warning
-          return
-        } else {
-          warning.classList.add('hidden'); // Hide the warning
-        }
+      // Checks if the domain name is greater than 1 character
 
-        // Checks the domain extention against the list of TLDs
-        if (domainList.includes(domainExtention)) {
-          warning.classList.add('hidden'); // Hide the warning
+      if (domainName.length < 2) {
+        warning.classList.remove('hidden'); // Show the warning
+        return
+      } else {
+        warning.classList.add('hidden'); // Hide the warning
+      }
 
-          // Checks if the domain is taken by using SOA records
-          const fetchDomain = () => {
-            fetch(`https://cloudflare-dns.com/dns-query?type=SOA&name=${domainName}${domainExtention}`,
+      // Checks the domain extention against the list of TLDs
+      if (domainList.includes(domainExtention)) {
+        warning.classList.add('hidden'); // Hide the warning
+
+        // Checks if the domain is taken by using SOA records
+        const fetchDomain = () => {
+          fetch(`https://cloudflare-dns.com/dns-query?type=SOA&name=${domainName}${domainExtention}`,
+            {
+              headers:
               {
-                headers:
-                {
-                  Accept: 'application/dns-json'
-                }
-              })
-              .then(response => response.json())
-              .then(response => {
-                let hasSOA = false;
-                // Checks if the response has an SOA record
-                if (response.Answer) {
-                  hasSOA = response.Answer.some(record => record.type === 6);
-                }
+                Accept: 'application/dns-json'
+              }
+            })
+            .then(response => response.json())
+            .then(response => {
+              let hasSOA = false;
+              // Checks if the response has an SOA record
+              if (response.Answer) {
+                hasSOA = response.Answer.some(record => record.type === 6);
+              }
 
-                const element = document.getElementById('domain-availability'); // Select the element
-                element.classList.add(hasSOA ? 'domain-taken' : 'domain-free'); // If the domain has SOA record, add the domain-taken class, else add the domain-free class
+              const element = document.getElementById('domain-availability'); // Select the element
+              element.classList.add(hasSOA ? 'domain-taken' : 'domain-free'); // If the domain has SOA record, add the domain-taken class, else add the domain-free class
 
-                if (hasSOA) {
-                  element.innerHTML = `<span class='taken'>Unfortunately</span>, ${domainName}${domainExtention} is not available`;
-                } else {
-                  element.innerHTML = `<span class='available'>Congratulations</span>, ${domainName}${domainExtention} is available`;
-                }
-              })
-              .catch(error => {
-                console.log(error);
-              });
-          };
-          fetchDomain() // Call the function
-        } else {
-          warning.classList.remove('hidden'); // if domain list does not include the domain extention, show the warning
-        }
-      });
-    }
+              if (hasSOA) {
+                element.innerHTML = `<span class='taken'>Unfortunately</span>, ${domainName}${domainExtention} is not available`;
+              } else {
+                element.innerHTML = `<span class='available'>Congratulations</span>, ${domainName}${domainExtention} is available`;
+              }
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        };
+        fetchDomain() // Call the function
+      } else {
+        warning.classList.remove('hidden'); // if domain list does not include the domain extention, show the warning
+      }
+    });
   }, [])
   return (
     <section className="section-domain-search">
